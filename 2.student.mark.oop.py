d2da1072d61450ab4f5e831f3197e290548a387f
Giang Nguyễn <gianglt.23bi14138@usth.edu.vn>
2024-12-13 21:27:46 +0700

    Update work

--- 2.student.mark.oop.py
+++ 2.student.mark.oop.py
@@ -1,6 +1,6 @@
 class student:
-    def __init__(self, person_id, name, dob):
-        self.id = person_id
+    def __init__(self, student_id, name, dob):
+        self.id = student_id
         self.name = name
         self.dob = dob
     def __str__(self):
@@ -13,9 +13,32 @@ class course:
     def __str__(self):
         return f"ID: {self.id}, Name: {self.name}"
 
-class Marks:
+class Mark:
     def __init__(self):
-        self.marks = {}
-
+        self.mark = {}
     def add_mark(self, course_id, student_id, mark):
-        
+        if course_id not in self.mark:
+            self.mark[course_id] = {}
+        self.marks[course_id][student_id] = mark
+    def course_mark(self, course_id):
+        return self.marks.get(course_id, None)
+
+class option:
+    def __init__(self):
+        self.student = []
+        self. course = []
+        self.mark = mark()
+    def input_student(self):
+        print("Input number of student: ")
+        count = int(input())
+        for _ in range(count):
+            print("Enter ID: ")
+            student_id = int(input())
+            print("Enter st name: ")
+            name = str(input())
+            print("DoB: ")
+            dob = str(input())
+            self.student.append(student(student_id, name, dob)
+    def input_course(self):
+        print("")
+        course_count = int(input())
